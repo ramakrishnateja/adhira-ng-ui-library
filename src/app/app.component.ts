f88c0cf6b6ac02804a861b6f0111232e08638240
Teja Guntu <Teja.Guntu@ecoinsight.corp>
2017-11-27 16:05:59 -0500

    added a proper behavior for selectedValue input field for radiobuttons component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   applicationTypeCategories: Array<SelectableItem<string>> = new Array<SelectableItem<string>>();
-  areaType: string;
+  areaType = 'indoor';
   constructor() {
     this.applicationTypeCategories.push(new SelectableItem<string>('indoor', 'Indoor'));
     this.applicationTypeCategories.push(new SelectableItem<string>('outdoor', 'Outdoor'));
+    this.applicationTypeCategories.push(new SelectableItem<string>('extraTerrestrial', 'E.T'));
   }
 
   applicationTypeCategorySelectionChanged(category: SelectableItem<string>): void {
     this.areaType = category.value;
+  }
+
+  clearSelection(): void {
+    this.areaType = '';
+  }
+
+  displaySelectedAreaType(): void {
+    console.log(this.areaType);
   }
 }
--- src/app/library/bs-radio-buttons/bs-radio-buttons.component.ts
+++ src/app/library/bs-radio-buttons/bs-radio-buttons.component.ts
@@ -1,15 +1,14 @@
 import { SelectableItem } from './../../models/selectable-item';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-bs-radio-buttons',
   templateUrl: './bs-radio-buttons.component.html',
   styleUrls: ['./bs-radio-buttons.component.css']
 })
-export class BsRadioButtonsComponent implements OnInit {
-
+export class BsRadioButtonsComponent implements OnInit, OnChanges {
   @Input() options: Array<SelectableItem<any>>;
-  @Input() selectedOption: any;
+  @Input() selectedValue: any;
   @Input() selectedClassName: string;
   @Input() defaultClassName: string;
   @Output() optionChanged = new EventEmitter<SelectableItem<any>>();
@@ -18,7 +17,7 @@ export class BsRadioButtonsComponent implements OnInit {
   onOptionSelected(option: SelectableItem<any>) {
     this.options.forEach(o => o.isSelected = false);
     option.isSelected = true;
-    this.selectedOption = option;
+    this.selectedValue = option.value;
     if (this.optionChanged) {
       this.optionChanged.emit(option);
     }
@@ -27,4 +26,28 @@ export class BsRadioButtonsComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      if (propName && (propName === 'options' || propName === 'selectedValue')) {
+        this.handleSelectedOption(this.selectedValue, this.options);
+      }
+    }
+  }
+
+  handleSelectedOption(selectedOption: any, options: Array<SelectableItem<any>>) {
+    if (selectedOption && options && options.length > 0) {
+      const option = options.find(o => o.value === selectedOption);
+      if (option) {
+        option.isSelected = true;
+      } else {
+        this.clearSelections();
+      }
+    } else if (options && options.length > 0) {
+      this.clearSelections();
+    }
+  }
+
+  clearSelections(): void {
+    this.options.forEach(o => o.isSelected = false);
+  }
 }
